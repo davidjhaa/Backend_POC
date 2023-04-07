47560182877d3936c4cfac429fc96ac5be617c4d
davidjhaa4u <davidjhaa4u@gmail.com>
2023-04-07 21:01:57 +0530

    server creation completed

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req,res)=>{
+    console.log("request has been made from browser to server");
+    console.log(req.method);
+    console.log(req.url);
+
+    res.setHeader('content-type', 'text/html');
+    // res.write('<h1>Hello boyz :)</h1>');
+    // res.write('<h1>How u doin :)</h1>');
+
+    let path = "./views";
+    switch(req.url){
+        case '/':
+            path += '/index.html';
+            break;
+        case '/about':
+            path += '/about.html';
+            break;
+        default :
+            path += '/404.html';
+            break;
+    }
+
+    fs.readFile(path, (err, fileData)=>{
+        if(err)
+            console.log(err);
+        else
+            res.end(fileData);
+    })
+
+});
+
+server.listen(5000, 'localhost', function(){
+    console.log('server is listening on port 5000');
+});
