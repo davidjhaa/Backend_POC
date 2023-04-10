fb8b91b950943f367d25d340aa5aef42132306c3
davidjhaa4u <davidjhaa4u@gmail.com>
2023-04-10 23:51:28 +0530

    express introduction

--- server.js
+++ server.js
@@ -14,12 +14,21 @@ const server = http.createServer((req,res)=>{
     switch(req.url){
         case '/':
             path += '/index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += '/about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            path += '/about.html';
+            res.setHeader('location', '/about')
+            res.statusCode = 301;
+            res.end();
             break;
         default :
             path += '/404.html';
+            res.statusCode = 404;
             break;
     }
 
